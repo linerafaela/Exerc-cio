1cbd4d35fc8b930861e265b8464f36f6beee27eb
linerafaela <128156711+linerafaela@users.noreply.github.com>
2024-03-06 09:50:46 -0300

    Add files via upload

---
+++ lacofor.py
@@ -0,0 +1,20 @@
+nomes = ['Thauane', 'Aline','Luana']
+for nome in nomes:
+    print(nome)
+
+print("#####################")
+
+
+soma = 0
+for i in range(1,200):
+    soma = soma +1
+    print(soma)
+
+print("#####################")
+
+
+n = 5
+fatorial = 1
+for i in range(1, n+1):
+    fatorial = fatorial * i
+print(f"O fatorial de {n} é {fatorial}")
